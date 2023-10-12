0d9e26fe31ba458c309c86b76108360588f76212
elau <elauso@gmail.com>
2023-10-12 13:26:52 -0300

    Implementando singleton pattern

--- src/main/kotlin/net/elau/example/designpatternsexamples/singleton/Bootstrap.kt
+++ src/main/kotlin/net/elau/example/designpatternsexamples/singleton/Bootstrap.kt
@@ -3,9 +3,17 @@ package net.elau.example.designpatternsexamples.singleton
 
 fun main(args: Array<String>) {
 
-    val configuration = Configuration.INSTANCE
-    println("Empty configuration: ${configuration.properties}")
+    val config1 = Configuration.INSTANCE
 
-    configuration.properties = mapOf("xyz" to "blah", "abc" to "bleh")
-    println("Loaded configuration: ${configuration.properties}")
+    val config2 = Configuration.INSTANCE
+    config2.properties = mapOf("xyz" to "blah", "abc" to "bleh")
+
+    if (config1 == config2) {
+        println("Two objects are same")
+    } else {
+        println("Two objects are not same")
+    }
+
+    println("config1: ${config1.properties}")
+    println("config2: ${config2.properties}")
 }
